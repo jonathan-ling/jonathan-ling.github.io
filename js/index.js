50c165f77bc1eaec7f481162e3e15a9c0cc2df77
jonathan-ling <jonling77@gmail.com>
2020-01-05 23:07:52 -0800

    Added initial codebase

---
+++ js/index.js
@@ -0,0 +1,50 @@
+// configure AngularJS routing for Single Page Application functionality
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "home.html",
+        resolve: {
+         init: function() {
+           $('.navbar-nav li').removeClass('active');
+           $('#my-nav-home').addClass('active');
+         }
+       }
+    })
+    .when("/about", {
+        templateUrl : "about.html",
+        resolve: {
+         init: function() {
+           $('.navbar-nav li').removeClass('active');
+           $('#my-nav-about').addClass('active');
+         }
+       }
+    })
+    .when("/resume", {
+        templateUrl : "resume.html",
+        resolve: {
+         init: function() {
+           $('.navbar-nav li').removeClass('active');
+           $('#my-nav-resume').addClass('active');
+         }
+       }
+    })
+    .when("/projects", {
+        templateUrl : "projects.html",
+        resolve: {
+         init: function() {
+           $('.navbar-nav li').removeClass('active');
+           $('#my-nav-projects').addClass('active');
+         }
+       }
+    });
+});
+app.config(['$locationProvider', function($locationProvider) {
+    $locationProvider.hashPrefix('');
+}]);
+
+// Other
+u = atob('QWRtYnB4bWJ2cG1PdlJIYnBGV2I=').split('').reverse().join('');
+d = atob('c3RhbmZvcmQuZWR1');
+send = atob(u)+d;
